5ed7f0abfb0bdab72542879d4699f18b58942ae8
Arindam <abatabyal@gmail.com>
2019-07-07 23:50:18 +0530

    get all btc pairs

--- src/main/java/com/crypto/currency/DepthEvents.java
+++ src/main/java/com/crypto/currency/DepthEvents.java
@@ -3,12 +3,21 @@
  */
 package com.crypto.currency;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.market.TickerPrice;
+import com.crypto.currency.models.BtcPairs;
+import com.crypto.currency.models.BtcPairsBulkRepository;
+import com.crypto.currency.models.BtcPairsRepositoy;
 
 /**
  * @author tony
@@ -16,6 +25,12 @@ import com.binance.api.client.BinanceApiRestClient;
  */
 @RestController
 public class DepthEvents {
+	
+	@Autowired
+	BtcPairsRepositoy btcPairsRepository;
+	
+	@Autowired
+	BtcPairsBulkRepository btcPairsBulkRepository;
 	
 	BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance("BJDUNS0V5xYobz1wIhDYpPUI9YxvS8EG8PyxWMSYb48e3hfm04hR8Kdm5IhY2ZBM","4I6Ism5kvlDrU748pZgDd5MqiLGLK41uF0ZIZ6VrdjwVhQQ8x8gMdPga1tN1NT4X");
 	BinanceApiRestClient client = factory.newRestClient();
@@ -25,6 +40,17 @@ public class DepthEvents {
 		long serverTime = client.getServerTime();
 		System.out.println(serverTime);
 		System.out.println("Now the output is redirected");
+	}
+	
+	@RequestMapping(value = "/saveAllPairs", method= RequestMethod.POST)
+	public <S> void saveAllPairs() {
+		List<TickerPrice> allPrices = client.getAllPrices();
+		List<BtcPairs> btcPairList = new ArrayList<BtcPairs>();
+		for (TickerPrice tickerPrice : allPrices) {
+			System.out.println(tickerPrice.getSymbol());
+			btcPairList.add(new BtcPairs(tickerPrice.getSymbol()));	
+		}
+		btcPairsBulkRepository.saveAll(btcPairList);
 	}
 	
 
